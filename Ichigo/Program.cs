9fbd9a61d7fd859340b9f8d95b61b9580985a4db
Pieter Germishuys| <pieter@pieterg.com>
2018-06-11 21:32:45 +0200

    Add initial solution

---
+++ Ichigo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ichigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
